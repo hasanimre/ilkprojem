f00b0db8a9f5d946d14d1ece90b53034e22ede91
Ahmet Hasan İmre <cimedo25@gmail.com>
2025-09-02 16:48:21 +0300

    Add files via upload

---
+++ Hesapmakinesi.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+//my first project...
+using namespace std;
+
+int main(){
+	double x, y ;
+	char op;
+		cout << "Ahmet'in hesap makinesi : " << endl << " +  -  *  / " << endl;
+		cout << "ilk sayi : " ;
+		cin >> x ;
+		cout << "islem ? ";
+		cin >> op;
+		cout << "ikinci sayi : " ;
+		cin >> y ;
+		switch(op){
+	
+			case '+' : cout << "Sonucunuz : " << x+y ; break;
+			case '-' : cout << "Sonucunuz : " << x-y ; break; 
+			case '*' : cout << "Sonucunuz : " << x*y ; break;
+			case '/' : 
+				if (y == 0){
+				cout << "Sonuc tanýmsýz." ;
+				}
+				else{
+				cout << "Sonucunuz : " << x/y;
+				}
+				break;
+				default: cout << "Gecersiz islem !" ;
+	
+	
+}
+	
+	return 0;
+}
